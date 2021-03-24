e4435047aeaaf05f6feb910426be7dfca2599214
Seongho Jeon <satosily@gmail.com>
2021-03-24 17:47:17 +0900

    first commit

---
+++ singleton.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+class Singleton
+{
+    private:
+        static Singleton* instance;
+        Singleton();
+
+    public:
+        static Singleton* getInstance();
+};
+
+Singleton::Singleton()
+{}
+
+Singleton* Singleton::instance = 0;
+
+Singleton* Singleton::getInstance()
+{
+    if (instance == 0)
+    {
+        instance = new Singleton();
+    }
+
+    return instance;
+}
+
+int main()
+{
+    Singleton* s = Singleton::getInstance(); 
+    Singleton* r = Singleton::getInstance();
+
+    std::cout << s << std::endl;
+    std::cout << r << std::endl;
+}
